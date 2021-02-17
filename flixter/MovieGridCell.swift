ceb96299c70fe83f45076e1b0559f8a7837f9773
Truc Phan <truc.t.phan@sjsu.edu>
2021-02-16 23:35:09 -0800

    Add movie detail screen, tab bar, and grid view for similar movies.

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Truc Phan on 2/13/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
